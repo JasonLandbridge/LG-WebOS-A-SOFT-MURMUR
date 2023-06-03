21f8e52f03ab46f49aaa7a16ce1fe22a85f9de3f
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-06-03 16:29:41 +0200

    build: Added Nuxt-Quasar UI framework

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
+import {defineNuxtConfig} from 'nuxt/config';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    srcDir: 'src',
+    modules: [
+        'nuxt-quasar-ui'
+    ],
+    quasar: {
+        iconSet: 'mdi-v7',
+    }
 })

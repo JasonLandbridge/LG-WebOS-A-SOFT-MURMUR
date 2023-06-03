aa642886d1b879ea2968f7f4e334fb3ef3b4892f
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-06-03 18:52:59 +0200

    build: Added VueUse and Typescript NPM packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,32 @@
 import {defineNuxtConfig} from 'nuxt/config';
+import {fileURLToPath} from "url";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     srcDir: 'src',
     modules: [
-        'nuxt-quasar-ui'
+        'nuxt-quasar-ui',
+        '@vueuse/nuxt',
+        '@vueuse/sound/nuxt',
     ],
     quasar: {
         iconSet: 'mdi-v7',
+    },
+    /**
+     * Sounds: https://github.com/vueuse/sound
+     */
+    sound: {
+        sounds: {
+            scan: true,
+        }
     },
     /*
      ** Global CSS: https://nuxt.com/docs/api/configuration/nuxt-config#css
      */
     css: ['@/assets/style/style.scss'],
+    alias: {
+        // Doc: https://nuxt.com/docs/api/configuration/nuxt-config#alias
+        '@img': fileURLToPath(new URL('./src/assets/img/', import.meta.url)),
+        '@sounds': fileURLToPath(new URL('./src/assets/sounds/', import.meta.url)),
+    },
 })

89bc1270ee2b3db41a7f59895c47cf06b2e689a1
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-06-03 18:57:23 +0200

    refactor: added the rest of the sound types to SoundTypeEnum

--- src/types/SoundTypeEnum.ts
+++ src/types/SoundTypeEnum.ts
@@ -1,6 +1,15 @@
 enum SoundType {
     None = 'none',
     Rain = 'rain',
+    Birds = 'birds',
+    Crickets = 'crickets',
+    Fire = 'fire',
+    People = 'people',
+    SingingBowl = 'sbowl',
+    Thunder = 'thunder',
+    Waves = 'waves',
+    WhiteNoise = 'whitenoise',
+    Wind = 'wind',
 }
 
 export default SoundType;

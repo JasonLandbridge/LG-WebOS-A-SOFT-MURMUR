55e41af7551fed60b96b7c83d4d4b308bc9b5284
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-06-09 13:02:51 +0200

    refactor: Moved sound images to public folder in scss

--- src/components/SoundTypeImage.vue
+++ src/components/SoundTypeImage.vue
@@ -29,64 +29,64 @@ defineProps<{
         background-repeat: no-repeat;
 
         &.img-fill-birds {
-            background-image: url('~/assets/img/sounds/birds-fill.png');
+            background-image: url('img/sounds/birds-fill.png');
         }
         &.img-outline-birds {
-            background-image: url('~/assets/img/sounds/birds-outline.png');
+            background-image: url('img/sounds/birds-outline.png');
         }
         &.img-fill-crickets {
-            background-image: url('~/assets/img/sounds/crickets-fill.png');
+            background-image: url('img/sounds/crickets-fill.png');
         }
         &.img-outline-crickets {
-            background-image: url('~/assets/img/sounds/crickets-outline.png');
+            background-image: url('img/sounds/crickets-outline.png');
         }
         &.img-fill-fire {
-            background-image: url('~/assets/img/sounds/fire-fill.png');
+            background-image: url('img/sounds/fire-fill.png');
         }
         &.img-outline-fire {
-            background-image: url('~/assets/img/sounds/fire-outline.png');
+            background-image: url('img/sounds/fire-outline.png');
         }
         &.img-fill-people {
-            background-image: url('~/assets/img/sounds/people-fill.png');
+            background-image: url('img/sounds/people-fill.png');
         }
         &.img-outline-people {
-            background-image: url('~/assets/img/sounds/people-outline.png');
+            background-image: url('img/sounds/people-outline.png');
         }
         &.img-fill-rain {
-            background-image: url('~/assets/img/sounds/rain-fill.png');
+            background-image: url('img/sounds/rain-fill.png');
         }
         &.img-outline-rain {
-            background-image: url('~/assets/img/sounds/rain-outline.png');
+            background-image: url('img/sounds/rain-outline.png');
         }
         &.img-fill-sbowl {
-            background-image: url('~/assets/img/sounds/sbowl-fill.png');
+            background-image: url('img/sounds/sbowl-fill.png');
         }
         &.img-outline-sbowl {
-            background-image: url('~/assets/img/sounds/sbowl-outline.png');
+            background-image: url('img/sounds/sbowl-outline.png');
         }
         &.img-fill-thunder {
-            background-image: url('~/assets/img/sounds/thunder-fill.png');
+            background-image: url('img/sounds/thunder-fill.png');
         }
         &.img-outline-thunder {
-            background-image: url('~/assets/img/sounds/thunder-outline.png');
+            background-image: url('img/sounds/thunder-outline.png');
         }
         &.img-fill-waves {
-            background-image: url('~/assets/img/sounds/waves-fill.png');
+            background-image: url('img/sounds/waves-fill.png');
         }
         &.img-outline-waves {
-            background-image: url('~/assets/img/sounds/waves-outline.png');
+            background-image: url('img/sounds/waves-outline.png');
         }
         &.img-fill-whitenoise {
-            background-image: url('~/assets/img/sounds/whitenoise-fill.png');
+            background-image: url('img/sounds/whitenoise-fill.png');
         }
         &.img-outline-whitenoise {
-            background-image: url('~/assets/img/sounds/whitenoise-outline.png');
+            background-image: url('img/sounds/whitenoise-outline.png');
         }
         &.img-fill-wind {
-            background-image: url('~/assets/img/sounds/wind-fill.png');
+            background-image: url('img/sounds/wind-fill.png');
         }
         &.img-outline-wind {
-            background-image: url('~/assets/img/sounds/wind-outline.png');
+            background-image: url('img/sounds/wind-outline.png');
         }
     }
 }
